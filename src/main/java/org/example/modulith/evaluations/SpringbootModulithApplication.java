1e3b85912c527086286c5cd6c34c8e668b21000c
Tech Pro Prabeen Pati <techprobeen@gmail.com>
2025-05-16 21:17:47 +0530

    Springboot Modulith Application

---
+++ src/main/java/org/example/modulith/evaluations/SpringbootModulithApplication.java
@@ -0,0 +1,13 @@
+package org.example.modulith.evaluations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootModulithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootModulithApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/modulith/evaluations/SpringbootModulithApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.modulith.evaluations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootModulithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
